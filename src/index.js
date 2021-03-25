87e336401706ab65db225e915860593c6d865847
mftver <m.vermulst@student.avans.nl>
2021-03-25 13:13:10 +0100

    attempt to generate grid

--- src/index.js
+++ src/index.js
@@ -2,3 +2,18 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import  './css/main.css';
 
 import { Tooltip, Toast, Popover } from 'bootstrap';
+
+
+let table = document.querySelector("table");
+generateTable(table);
+
+function generateTable(table) {
+    for (let index = 0; index < 15; index++) {
+        let row = table.insertRow();
+        for (let index = 0; index < 15; index++) {
+            let cell = row.insertCell();
+            let text = document.createTextNode("hello world");
+            cell.appendChild(text);
+        }
+    }
+}
