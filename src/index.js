35b665d9e3285f24c41d08797dd4e93ffcfe576d
Job <LordChunk@users.noreply.github.com>
2021-03-25 14:01:26 +0100

    Added draggable square

--- src/index.js
+++ src/index.js
@@ -1,19 +1,52 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
-import  './css/main.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./css/main.css";
 
-import { Tooltip, Toast, Popover } from 'bootstrap';
+let isDragging = false;
 
+window.onload = () => {
+  // Generate table
+  let table = document.createElement("table");
+  document.body.appendChild(table);
+  generateTable(table);
 
-let table = document.querySelector("table");
-generateTable(table);
+  // Generate draggable element
+  let draggableElement = document.createElement('div');
+  draggableElement.setAttribute('id', 'draggable');
+  draggableElement.textContent = 'Drag me!';
+  document.body.appendChild(draggableElement);
+};
 
 function generateTable(table) {
-    for (let index = 0; index < 15; index++) {
-        let row = table.insertRow();
-        for (let index = 0; index < 15; index++) {
-            let cell = row.insertCell();
-            let text = document.createTextNode("hello world");
-            cell.appendChild(text);
-        }
+  for (let i = 0; i < 15; i++) {
+    let row = table.insertRow();
+    for (let j = 0; j < 15; j++) {
+      let cell = row.insertCell();
+      let text = document.createTextNode(`${i}, ${j}`);
+      cell.setAttribute("data-coord-x", j);
+      cell.setAttribute("data-coord-y", i);
+      cell.appendChild(text);
     }
+  }
+}
+
+// Check if user clicks on draggable item
+document.onmousedown = (e) => {
+  let clickedElement = e.target;
+  if (clickedElement.id === "draggable") {
+    isDragging = true;
+  }
+};
+document.onmouseup = () => {
+  isDragging = false;
+}
+
+// Event handler for moving item while in drag mode
+document.onmousemove = (e) => {
+  if(isDragging) {
+    let clickedElement = document.getElementById('draggable');
+    
+    clickedElement.style.position = 'fixed';
+    clickedElement.style.top = (e.pageY - (clickedElement.clientHeight/2)) +"px";
+    clickedElement.style.left = (e.pageX - (clickedElement.clientWidth/2)) +"px";
+  }
 }
