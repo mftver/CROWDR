ce2f4adb1e439f86b5961cd6ce9a84a4df37eed8
mftver <m.vermulst@student.avans.nl>
2021-03-29 14:12:18 +0200

    added logic for placement of elements

--- src/js/placement-logic.js
+++ src/js/placement-logic.js
@@ -3,14 +3,40 @@ function closeTableCell(tableCell) {
   tableCell.classList.add('selected-cell');
 }
 
+function canPlace(xCoordinate, yCoordinate) {
+  const tablecell = document.querySelector(`[data-coord-x='${xCoordinate}'][data-coord-y='${yCoordinate}']`);
+
+  if (xCoordinate < 0 || xCoordinate > 14 || yCoordinate < 0 || yCoordinate > 14) {
+    return false;
+  } if (tablecell.classList.contains('selected-cell')) {
+    return false;
+  }
+
+  return true;
+}
+
+function placeObject(coordinates) {
+  coordinates.forEach((element) => {
+    closeTableCell(document.querySelector(`[data-coord-x='${element[0]}'][data-coord-y='${element[1]}']`));
+  });
+}
+
 function place3x3(tableCell) {
   const yCoordinate = tableCell.getAttribute('data-coord-y') - 1;
   const xCoordinate = tableCell.getAttribute('data-coord-x') - 1;
 
+  const possitionsToBePlaced = new Array(0);
+
   for (let i = 0; i < 3; i += 1) {
     for (let j = 0; j < 3; j += 1) {
-      closeTableCell(document.querySelector(`[data-coord-x='${xCoordinate + i}'][data-coord-y='${yCoordinate + j}']`));
+      if (canPlace(xCoordinate + i, yCoordinate + j)) {
+        possitionsToBePlaced.push([xCoordinate + i, yCoordinate + j]);
+      }
     }
+  }
+
+  if (possitionsToBePlaced.length === 9) {
+    placeObject(possitionsToBePlaced);
   }
 }
 
@@ -22,8 +48,14 @@ function place1x2(tableCell) {
   const yCoordinate = tableCell.getAttribute('data-coord-y') - 0;
   const xCoordinate = tableCell.getAttribute('data-coord-x') - 0;
 
+  const possitionsToBePlaced = new Array(0);
+
   for (let i = 0; i < 2; i += 1) {
-    closeTableCell(document.querySelector(`[data-coord-x='${xCoordinate + i}'][data-coord-y='${yCoordinate}']`));
+    possitionsToBePlaced.push([xCoordinate + i, yCoordinate]);
+  }
+
+  if (possitionsToBePlaced.length === 2) {
+    placeObject(possitionsToBePlaced);
   }
 }
 
@@ -31,8 +63,14 @@ function place2x1(tableCell) {
   const yCoordinate = tableCell.getAttribute('data-coord-y') - 0;
   const xCoordinate = tableCell.getAttribute('data-coord-x') - 0;
 
+  const possitionsToBePlaced = new Array(0);
+
   for (let i = 0; i < 2; i += 1) {
-    closeTableCell(document.querySelector(`[data-coord-x='${xCoordinate}'][data-coord-y='${yCoordinate + i}']`));
+    possitionsToBePlaced.push([xCoordinate, yCoordinate + i]);
+  }
+
+  if (possitionsToBePlaced.length === 2) {
+    placeObject(possitionsToBePlaced);
   }
 }
 
@@ -40,8 +78,14 @@ function place1x3(tableCell) {
   const yCoordinate = tableCell.getAttribute('data-coord-y');
   const xCoordinate = tableCell.getAttribute('data-coord-x') - 1;
 
+  const possitionsToBePlaced = new Array(0);
+
   for (let i = 0; i < 3; i += 1) {
-    closeTableCell(document.querySelector(`[data-coord-x='${xCoordinate + i}'][data-coord-y='${yCoordinate}']`));
+    possitionsToBePlaced.push([xCoordinate + i, yCoordinate]);
+  }
+
+  if (possitionsToBePlaced.length === 3) {
+    placeObject(possitionsToBePlaced);
   }
 }
 
