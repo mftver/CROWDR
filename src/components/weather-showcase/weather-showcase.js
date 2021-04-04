82fb54b4034a1e3fa2ac2f27567fc91a61fc29de
Job <LordChunk@users.noreply.github.com>
2021-04-04 17:56:47 +0200

    Weather type can now be force changed

--- src/components/weather-showcase/weather-showcase.js
+++ src/components/weather-showcase/weather-showcase.js
@@ -1,13 +1,29 @@
+import html from './weather-showcase.html';
+import WeatherApiService from '../../js/weather-api-service'
+
 export default class WeatherShowcase extends HTMLElement {
   constructor() {
     super();
     document.addEventListener('weatherupdate', (e) => this.Update(e.detail));
   }
 
-  Update(e) {
-    this.innerHTML = '';
+  connectedCallback() {
+    this.innerHTML = html;
+
+    this.addEventListener('click', (e) => {
+      const weatherType = e.target.getAttribute('data-type');
+      WeatherApiService.UpdateWeather(weatherType);
+    });
+  }
+
+  Update(weatherType) {
+    this.querySelector('img').remove();
     const img = new Image(50, 50);
-    img.src = `https://openweathermap.org/img/wn/${e.weather[0].icon}@2x.png`;
+    if (weatherType === 'sunny') {
+      img.src = 'https://openweathermap.org/img/wn/01d@2x.png';
+    } else {
+      img.src = 'https://openweathermap.org/img/wn/09d@2x.png';
+    }
     this.appendChild(img);
   }
 }
--- src/js/weather-api-service.js
+++ src/js/weather-api-service.js
@@ -4,22 +4,30 @@ class WeatherApiService {
     setInterval(this.Update, 60000);
   }
 
+  WeatherType = 'sunny';
+
   // eslint-disable-next-line class-methods-use-this
   Update() {
     const url = 'https://api.openweathermap.org/data/2.5/weather?q=Den Bosch,NL&APPID=fc24cd69b564813ec6a99ab022992c76';
     fetch(url)
       .then((response) => response.json())
       .then((data) => {
-        document.dispatchEvent(new CustomEvent('weatherupdate', {
-          bubbles: true,
-          cancelable: false,
-          composed: true,
-          detail: data,
-        }));
+        if (data.weather[0].id >= 800) {
+          this.UpdateWeather('sunny');
+        } else {
+          this.UpdateWeather('rainy');
+        }
       })
       .catch((error) => {
         console.error(error);
       });
+  }
+
+  UpdateWeather(weatherType) {
+    this.weatherType = weatherType;
+    document.dispatchEvent(new CustomEvent('weatherupdate', {
+      detail: weatherType,
+    }));
   }
 }
 
