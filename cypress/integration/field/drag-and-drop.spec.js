4d7c69e6c01fbff8860544a36e8a4884482411b8
Job <LordChunk@users.noreply.github.com>
2021-04-04 16:01:21 +0200

    Finalised drag and drop tests

--- cypress/integration/field/drag-and-drop.spec.js
+++ cypress/integration/field/drag-and-drop.spec.js
@@ -25,10 +25,11 @@ context('Drag and drop testing', () => {
   });
 
   it('Cannot place more than configured number of tents', () => {
-    for (let i = 1; i <= 3; i++) {
+    for (let i = 1; i <= 6; i += 3) {
       cy.get('.draggable-container .Tent3x3').trigger('mousedown');
-      cy.get('[data-coord-x="1"][data-coord-y="1"]').trigger('mousemove');
+      cy.get(`[data-coord-x="1"][data-coord-y="${i}"]`).trigger('mousemove');
       cy.get('#dragging').trigger('mouseup');
     }
+    cy.get('.draggable-container .Tent3x3').should('not.be.visible')
   });
 });
