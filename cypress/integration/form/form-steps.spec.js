99238d2bd4e1f8b781d0ebafb89c212e38a860ba
Job <LordChunk@users.noreply.github.com>
2021-04-04 14:50:20 +0200

    Added food stand value validation

--- cypress/integration/form/form-steps.spec.js
+++ cypress/integration/form/form-steps.spec.js
@@ -8,7 +8,21 @@ context('Form validation', () => {
 
   it('Accepts default values', () => {
     for (let index = 0; index < 7; index += 1) {
-      cy.get('button[type="submit"]').click()
+      cy.get('button[type="submit"]').click();
     }
+
+    cy.location('href').should('include', '/#/grid/1');
   });
+
+  it("Doesn't allow more than 3 food stands", () => {
+    for (let index = 0; index < 6; index += 1) {
+      cy.get('button[type="submit"]').click();
+    }
+
+
+    cy.get('[name="Tent1x1"]').type(4);
+    cy.get('button[type="submit"]').click();
+
+    cy.get('.alert').should('contain.text', 'Het aantal eetkraampjes mag niet meer dan 3 zijn');
+  })
 });
